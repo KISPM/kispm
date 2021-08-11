62b313c0a9269d1425ff50aae576edaf65b90562
Kai Lyons <kailyons@pop-os.localdomain>
2021-08-11 10:51:27 -0600

    remove experimental shell

--- ffs.sh
+++
@@ -1,8 +0,0 @@
-regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
-string='http://www.google.com/test/link.php'
-if [ $string = $regex ]
-then 
-    echo "Link valid"
-else
-    echo "Link not valid"
-fi
